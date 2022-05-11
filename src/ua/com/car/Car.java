ea98cc88a9cd6dccfd12f37d9d0a38a9011fd0d5
bas0101 <a.s.bozhenko@gmail.com>
2022-05-11 14:06:46 +0300

    comment toString commit

---
+++ src/ua/com/car/Car.java
@@ -0,0 +1,28 @@
+package ua.com.car;
+
+public class Car {
+    String name;
+    int yearOfProduction;
+    int price;
+    int weight;
+    Color color;
+
+    public Car(String name, int yearOfProduction, int price, int weight, Color color) {
+        this.name = name;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", yearOfProduction=" + yearOfProduction +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", color=" + color +
+                '}';
+    }
+}
---
+++ src/ua/com/car/CarManager.java
@@ -0,0 +1,21 @@
+package ua.com.car;
+
+
+
+public class CarManager {
+
+    public static void main(String[] args) {
+       // Color.valueOf("GREEN")
+        Car prius = new Car("Toyota Prius", 2008,
+                15000, 1200, Color.GREEN);
+
+        Car renualt = new Car( "Renault Laguna", 2000,
+                9700, 1600, Color.WHITE);
+
+        System.out.println(prius);
+        System.out.println(renualt);
+        System.out.println(renualt);
+
+    }
+
+}
